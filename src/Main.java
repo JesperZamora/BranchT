b325c67b4e4a7aea6b7bf25bbbcb819265f29b1a
JesperZamora <jesp78m6@stud.kea.dk>
2022-11-14 10:27:52 +0100

    new test added in main

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
 
-
     public static void main(String[] args) {
         Dog dog = new Dog();
         Cat cat = new Cat();
 
         dog.makeSound();
         cat.makeSound();
+
+        System.out.println(dog.eat("dog food"));
+        System.out.println(cat.eat("cat food"));
     }
 
 
